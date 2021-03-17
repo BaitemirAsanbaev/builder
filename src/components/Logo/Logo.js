59f3949b3dbbb55dad6dcb75ede514dacf977efe
Baitemir <ferrari170604@gmail.com>
2021-03-17 20:05:12 +0600

    stylized logo and title

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,11 @@
 import classes from "./Logo.module.css";
+import logo from "./logo.png"
 
 const Logo = () => {
     return ( 
         <div className = {classes.Logo}>
-            Logo
+            <img src = {logo} className = {classes.logoImg}/>
+            <h1 className = {classes.title}>Music maker</h1>
         </div>
      );
 }
