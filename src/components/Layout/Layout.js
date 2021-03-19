4fa01b60ee52b77af1b4245c6f8c5d92a91ff7b3
Baitemir <ferrari170604@gmail.com>
2021-03-19 15:03:17 +0600

    added backdrop component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,16 @@
 import classes from "./Layout.module.css";
 import Toolbar from "../Toolbar/Tooolbar";
 import Drawer from "../Drawer/Drawer";
+import Backdrop from "../UI/Backdrop/Backdrop";
 
 const Layout = () => {
     return ( 
         <div className = {classes.Layout}>
-            <Toolbar/>
-            <Drawer/>
+            <Backdrop/>
+            <div>
+                <Toolbar/>
+                <Drawer/>
+            </div>
         </div>
      );
 }
