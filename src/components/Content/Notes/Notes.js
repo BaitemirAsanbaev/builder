c265ea23f1df0689e628bdea63b9fd75d3168696
Baitemir <ferrari170604@gmail.com>
2021-03-26 16:06:26 +0600

    x trsaamslete

--- src/components/Content/Notes/Notes.js
+++ src/components/Content/Notes/Notes.js
@@ -1,15 +1,16 @@
 import classes from "./Notes.module.css";
 import note from "../../../images/note.png"
+import React from 'react'
 
 const Notes = ({type, fixed}) => {
     const types = {
-        c: {backgroundImage: `url(${note})`, width: '55px', height: '55px', margin: '10px 15px', transform: 'translateY(200px)', },
-        d: {backgroundImage: `url(${note})`, width: '55px', height: '55px', margin: '10px 15px', transform: 'translateY(180px)',},
-        e: {backgroundImage: `url(${note})`, width: '55px', height: '55px', margin: '10px 15px', transform: 'translateY(160px)',},
-        f: {backgroundImage: `url(${note})`, width: '55px', height: '55px', margin: '10px 15px', transform: 'translateY(140px)',},
-        g: {backgroundImage: `url(${note})`, width: '55px', height: '55px', margin: '10px 15px', transform: 'translateY(120px)',},
-        a: {backgroundImage: `url(${note})`, width: '55px', height: '55px', margin: '10px 15px', transform: 'translateY(100px)',},
-        b: {backgroundImage: `url(${note})`, width: '55px', height: '55px', margin: '10px 15px', transform: 'translateY(80px)',},
+        c: {backgroundImage: `url(${note})`, width: '55px', height: '55px', margin: '10px 15px', transform: 'translateY(200px) translateX(100px)', },
+        d: {backgroundImage: `url(${note})`, width: '55px', height: '55px', margin: '10px 15px', transform: 'translateY(180px) translateX(100px)',},
+        e: {backgroundImage: `url(${note})`, width: '55px', height: '55px', margin: '10px 15px', transform: 'translateY(160px) translateX(100px)',},
+        f: {backgroundImage: `url(${note})`, width: '55px', height: '55px', margin: '10px 15px', transform: 'translateY(140px) translateX(100px)',},
+        g: {backgroundImage: `url(${note})`, width: '55px', height: '55px', margin: '10px 15px', transform: 'translateY(120px) translateX(100px)',},
+        a: {backgroundImage: `url(${note})`, width: '55px', height: '55px', margin: '10px 15px', transform: 'translateY(100px) translateX(100px)',},
+        b: {backgroundImage: `url(${note})`, width: '55px', height: '55px', margin: '10px 15px', transform: 'translateY(80px) translateX(100px)',},
     }
     
     return ( 
@@ -19,4 +20,4 @@ const Notes = ({type, fixed}) => {
      );
 }
  
-export default Notes;
+export default React.memo(Notes);
