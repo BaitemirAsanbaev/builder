f7b23a746f705ffa360f520699279eae339d6285
Baitemir <ferrari170604@gmail.com>
2021-03-26 12:42:19 +0600

    filled note control component

--- src/components/Content/NotesControls/NoteControl/NoteControl.js
+++ src/components/Content/NotesControls/NoteControl/NoteControl.js
@@ -1,7 +1,14 @@
-const NoteControl = () => {
-    return ( <div>
-        
-    </div> );
+import classes from "./NoteControl.module.css";
+import Notes from "../../Notes/Notes";
+
+const NoteControl = ({add, remove, type}) => {
+    return (<div className={classes.NoteControl}>
+      <button className={classes.more} onClick={() => add(type)}>+</button>
+      <div className={classes.ingredient}>
+        <Notes type={type} fixed />
+      </div>
+      <button className={classes.less} onClick={() => remove(type)}>-</button>
+    </div>);
 }
  
 export default NoteControl;
