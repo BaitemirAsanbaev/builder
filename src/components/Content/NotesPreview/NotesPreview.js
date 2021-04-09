b726947cb674f157868c919e5bffde9f1db9615b
Baitemir <ferrari170604@gmail.com>
2021-04-09 14:43:43 +0600

    added save button

--- src/components/Content/NotesPreview/NotesPreview.js
+++ src/components/Content/NotesPreview/NotesPreview.js
@@ -1,12 +1,16 @@
 import classes from "./NotesPreview.module.css";
 import sheet from "../../../images/sheet.png";
 import Notes from "../Notes/Notes"
+import Button from "../../UI/Button/Button";
     
-    const NotesPreview = ({notes}) =>{
+    const NotesPreview = ({notes, playAll}) =>{
         const result = []
         for (const note in notes) {
                 result.push(<Notes key = {notes[note] + note} type = {notes[note]}/>)
         }
+
+
+
     return ( 
         <div className = {classes.NotesPreview}>
             <div
@@ -14,7 +18,10 @@ import Notes from "../Notes/Notes"
                 style = {{backgroundImage: `url(${sheet})`}}>
                 {result}
             </div>
-            <div className = {classes.total}>{notes.length} notes in total</div>
+            <div className={classes.extraPreview}>
+                <div className = {classes.total}>{notes.length} notes in total</div>
+                <Button onClick={()=>playAll()} modalButton = {true} green = {true}>Play</Button>
+            </div>
         </div>
      );
     }
