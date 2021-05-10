cfa0f6232fda51e7a1c7d05cdd25b930f704375c
Baitemir <ferrari170604@gmail.com>
2021-05-10 22:50:23 +0600

    stylized form and orders

--- src/components/Content/Auth/Form/Form.js
+++ src/components/Content/Auth/Form/Form.js
@@ -6,15 +6,15 @@ const Form = ({cancel, submit}) => {
     return ( <form onSubmit={submit}>
 
         <div className={classes.formdiv}>
+            <label htmlFor="">Composition name</label>
+            <input type="text" name="compName" id="compName" required/>
             <label htmlFor="">Name</label>
             <input type="text" name="name" id="name" required/>
             <label htmlFor="">Phone</label>
             <input type="phone" name="phone" id="phone" required pattern="0[0-9]{9}"/>
             <label htmlFor="">Address</label>
             <input type="text" name="address" id="address" required/>
-            <label htmlFor="">Composition name</label>
-            <input type="text" name="compName" id="compName" required/>
-            
+
             <div className={classes.btndiv}>
                 <Button green="true" mt = 'true'>Checkout</Button>
                 <Button onClick={cancel} red="true" mt = 'true'>Cancel</Button>
--- src/components/Content/Orders/Order/Order.js
+++ src/components/Content/Orders/Order/Order.js
@@ -1,19 +1,22 @@
 import classes from "./Order.module.css";
 
 //component
-const Order = ({ name, phone, address, notes, price }) => {
+const Order = ({ name, phone, address, notes, composition_name }) => {
 
   //returns notes in order
   const output = Object.keys(notes)
-    .map(note => <em>{notes[note]} - </em>);
+    .map(note => <em className={classes.notes}>{notes[note]} - </em>);
 
 
   //JSX
   return (
     <div className={classes.Order}>
-      <div>{name}, {phone}, {address}</div>
+      <strong>{composition_name}</strong>
       <div>{output}</div>
-      <strong>{price}</strong>
+      <br />
+      <div>{name} / {phone} / {address}</div>
+      <br/>
+      <hr />
     </div>
   );
 }
